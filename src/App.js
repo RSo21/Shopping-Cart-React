4e4c40793e6eb02bb1b944f2d7587b97cc9391b6
R <roksana_sobczak@wp.pl>
2020-05-28 21:47:10 +0200

    add footer and header styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        Product list
+      </main>
+      <footer>
+        All rights is reserved
+      </footer>
     </div>
   );
 }
