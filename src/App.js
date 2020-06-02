bfeef2d3f18ac027c0a057ce9535bb3e85f9d33d
R <roksana_sobczak@wp.pl>
2020-06-02 19:24:56 +0200

    cart component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React from 'react';
 import data from "./data.json";
 import Products from "./components/Products"
 import Filter from './components/Filter.jsx';
+import Cart from './components/Cart.jsx';
 
 class App extends React.Component{
   constructor(){
     super();
     this.state={
       products: data.products,
+      cartItems: [],
       size: "",
       sort: "",
     }
+  }
+
+  removeFromCart =(product) =>{
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter(x=>x._id !== product._id),
+    })
+    
+  }
+
+  addToCart = (product) =>{
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item=>{
+      if(item._id === product._id){
+        item.count++;
+        alreadyInCart = true
+      }
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count: 1})
+    }
+    this.setState ({
+      cartItems: cartItems,
+    })
   }
 
   filterProducts = (event) =>{
@@ -67,12 +94,15 @@ class App extends React.Component{
               filterProducts={this.filterProducts}
               sortProducts={this.sortProducts}
               ></Filter>
-              <Products products={this.state.products}></Products>
+              <Products 
+              products={this.state.products}
+              addToCart={this.addToCart}
+              ></Products>
             </div>
            <div className="sidebar">
-             Cart Items
+             <Cart cartItems={this.state.cartItems}
+             removeFromCart={this.removeFromCart} />
            </div>
-
           </div>
         </main>
         <footer>
@@ -80,9 +110,7 @@ class App extends React.Component{
         </footer>
       </div>
     );
-
   }
-
 }
 
 export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import formatCurrnecy from "../util"; 
+import formatCurrency from "../util"; 
 
 export default class Products extends Component {
   render() {
@@ -17,9 +17,9 @@ export default class Products extends Component {
                 </a>
                 <div className="product-price">
                   <div>
-                    {formatCurrnecy(product.price )}
+                    {formatCurrency(product.price )}
                   </div>
-                  <button className="button primary">Add  to Cart</button>
+                  <button onClick={()=>this.props.addToCart(product)}  className="button primary">Add  to Cart</button>
                 </div>
               </div>
             </li>
