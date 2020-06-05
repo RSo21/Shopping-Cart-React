6a5b35b36eba7acfa31a4ad7f4630133757719c8
R <roksana_sobczak@wp.pl>
2020-06-05 13:22:25 +0200

    checkout-form

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ class App extends React.Component{
     super();
     this.state={
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
       size: "",
       sort: "",
     }
   }
 
+  createOrder= (order) =>{
+    alert("Need to save order for " + order.name)
+  }
+
   removeFromCart =(product) =>{
     const cartItems = this.state.cartItems.slice();
     this.setState({
-      cartItems: cartItems.filter(x=>x._id !== product._id),
-    })
-    
+      cartItems: cartItems.filter((x)=>x._id !== product._id),
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x)=>x._id !== product._id))
+    );
   }
 
   addToCart = (product) =>{
@@ -38,6 +43,7 @@ class App extends React.Component{
     this.setState ({
       cartItems: cartItems,
     })
+    localStorage.setItem("cartItems", JSON.stringify(cartItems ));
   }
 
   filterProducts = (event) =>{
@@ -101,7 +107,9 @@ class App extends React.Component{
             </div>
            <div className="sidebar">
              <Cart cartItems={this.state.cartItems}
-             removeFromCart={this.removeFromCart} />
+             removeFromCart={this.removeFromCart} 
+             createOrder={this.createOrder}
+             />
            </div>
           </div>
         </main>
