12cad02a0c1c83802fa96971f886218a04536d2c
R <roksana_sobczak@wp.pl>
2020-06-07 10:27:39 +0200

    try beautify

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,49 +1,49 @@
 import React, { Component } from 'react';
-import formatCurrency from "../util"; 
+import formatCurrency from "../util";
 import Fade from "react-reveal/Fade";
 import Modal from "react-modal";
 import Zoom from "react-reveal/Zoom";
 export default class Products extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       product: null,
     }
 
   }
-  openModal = (product) =>{
-    this.setState({product})
+  openModal = (product) => {
+    this.setState({ product })
   };
 
-  closeModal = () =>{
-    this.setState({product: null})
+  closeModal = () => {
+    this.setState({ product: null })
 
   }
   render() {
-    const {product} = this.state
+    const { product } = this.state
     return (
       <div>
         <Fade bottom cascade={true}>
-        <ul className="products">
-          {this.props.products.map(product =>(
-            <li key={product._id}>
-              <div className="product">
-                <a href={"#" + product._id} onClick={()=> this.openModal(product)}>
-                  <img src={product.image} alt={product.title}/>
-                  <p>
-                    {product.title}
-                  </p>
-                </a>
-                <div className="product-price">
-                  <div>
-                    {formatCurrency(product.price )}
+          <ul className="products">
+            {this.props.products.map(product => (
+              <li key={product._id}>
+                <div className="product">
+                  <a href={"#" + product._id} onClick={() => this.openModal(product)}>
+                    <img src={product.image} alt={product.title} />
+                    <p>
+                      {product.title}
+                    </p>
+                  </a>
+                  <div className="product-price">
+                    <div>
+                      {formatCurrency(product.price)}
+                    </div>
+                    <button onClick={() => this.props.addToCart(product)} className="button primary">Add  to Cart</button>
                   </div>
-                  <button onClick={()=>this.props.addToCart(product)}  className="button primary">Add  to Cart</button>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>  
+              </li>
+            ))}
+          </ul>
         </Fade>
         {product && (
           <Modal isOpen={true} onRequestClose={this.closeModal}>
@@ -54,27 +54,27 @@ export default class Products extends Component {
                 <div className="product-details-description">
                   <stron><p>{product.title}</p></stron>
                   <p>{product.description}</p>
-                  <p>Available sizes {"  "} 
-                  {product.availableSizes.map(x=>(
-                    <span>{"  "} 
-                    <button className="button">{x}</button>
-                    </span>
-                  ))}
+                  <p>Available sizes {"  "}
+                    {product.availableSizes.map(x => (
+                      <span>{"  "}
+                        <button className="button">{x}</button>
+                      </span>
+                    ))}
                   </p>
                   <div className="product-price">
                     <div>
                       {formatCurrency(product.price)}
-                      {"  "} 
-                      <button 
-                      className="button primary" 
-                      onClick={()=>{
-                        this.props.addToCart(product)
-                        this.closeModal()
-                      }}>Add to cart</button>
-                      </div>
+                      {"  "}
+                      <button
+                        className="button primary"
+                        onClick={() => {
+                          this.props.addToCart(product)
+                          this.closeModal()
+                        }}>Add to cart</button>
                     </div>
                   </div>
                 </div>
+              </div>
             </Zoom>
           </Modal>
         )}
@@ -82,5 +82,3 @@ export default class Products extends Component {
     )
   }
 }
-
-  
